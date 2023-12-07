543767e8eaec7707f9998ef7b65ad5d1cd291b23
Annd <ssandreia30@gmail.com>
2023-12-07 16:36:54 -0300

    create overview component with city and date

---
+++ src/Overview.jsx
@@ -0,0 +1,12 @@
+function Overview() {
+  return (
+    <div className="Overview">
+      <h1 id="city">Sao Paulo</h1>
+      <ul>
+        <li id="date">Thursday, 7</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Overview;
--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,11 +1,13 @@
-import SearchForm from "./SearchForm"
+import Overview from "./Overview";
+import SearchForm from "./SearchForm";
 
 function WeatherApp() {
   return (
     <div className="weather-app px-5 rounded-4 shadow-lg">
       <SearchForm />
+      <Overview />
     </div>
-  )
+  );
 }
 
-export default WeatherApp
+export default WeatherApp;
