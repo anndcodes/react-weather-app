2157a9c5cb94231dd2c5fa18183f36812f299548
Annd <ssandreia30@gmail.com>
2023-12-07 16:24:09 -0300

    style: insert bootstrap classes on weather-app component

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -2,9 +2,9 @@ import SearchForm from "./SearchForm"
 
 function WeatherApp() {
   return (
-    <>
+    <div className="weather-app px-5 rounded-4 shadow-lg">
       <SearchForm />
-    </>
+    </div>
   )
 }
 
